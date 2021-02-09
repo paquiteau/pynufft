e6a7b1ceb72b37d69568f495d86619fc43f01223
Jyh-Miin Lin <jyhmiinlin@gmail.com>
2021-02-09 21:27:31 +0800

    use wavefront for pELL_spmvh_mCoil

--- src/re_subroutine.py
+++ src/re_subroutine.py
@@ -605,6 +605,77 @@ def cSpmvh():
     """
     
     R="""
+    
+        KERNEL void pELL_spmvh_mCoil(
+        const    unsigned int    Reps,             // number of coils
+        const    unsigned int    nRow,        // number of rows
+        const    unsigned int    prodJd,     // product of Jd
+        const    unsigned int    sumJd,     // sum of Jd
+        const    unsigned int    dim,           // dimensionality
+        GLOBAL_MEM const unsigned int *Jd,    // Jd
+        // GLOBAL_MEM const unsigned int *curr_sumJd,    // 
+        GLOBAL_MEM const unsigned int *meshindex,    // meshindex, prodJd * dim
+        GLOBAL_MEM const unsigned int *kindx,    // unmixed column indexes of all dimensions
+        GLOBAL_MEM const float2 *udata,    // interpolation data before Kronecker product
+        GLOBAL_MEM    float2    *k, 
+        GLOBAL_MEM float2 *res,
+        GLOBAL_MEM const float2 *input)   // y
+        {      
+        const unsigned int t = get_local_id(0);
+        const unsigned int vecWidth=${LL};
+        // Thread ID within wavefront
+        const unsigned int id = t & (vecWidth-1);
+        
+        // One row per wavefront
+        unsigned int vecsPerBlock=get_local_size(0)/vecWidth;
+        unsigned int myRow=(get_group_id(0)*vecsPerBlock) + (t/ vecWidth); // the myRow-th non-Cartesian sample
+        unsigned int m = myRow / Reps;
+        unsigned int nc = myRow - m * Reps;
+        
+        float2 zero;
+        zero.x = 0.0;
+        zero.y = 0.0;
+        
+        
+        if (myRow < nRow * Reps)
+        {
+        const unsigned int vecStart = 0; 
+        const unsigned int vecEnd =prodJd;             
+        float2  u=zero;
+        
+        for (unsigned int j = vecStart+id;  j<vecEnd; j += vecWidth)
+                        {    
+                        // now doing the first dimension
+                        unsigned int index_shift = m * sumJd;
+                        // unsigned int tmp_sumJd = 0;
+                        unsigned int J = Jd[0];
+                        unsigned int index =    index_shift +  meshindex[dim*j + 0];
+                        unsigned int col = kindx[index] ;
+                        float2 spdata = udata[index];
+                        index_shift += J; 
+                        for (unsigned int dimid = 1; dimid < dim; dimid ++ )
+                                {
+                                J = Jd[dimid];
+                                index =   index_shift + meshindex[dim*j + dimid];   // the index of the partial ELL arrays *kindx and *udata
+                                col += kindx[index];// + 1  ;                                            // the column index of the current j
+                                float tmp_x = spdata.x;
+                                float2 tmp_udata = udata[index];
+                                spdata.x = tmp_x * tmp_udata.x - spdata.y * tmp_udata.y;                            // the spdata of the current j
+                                spdata.y = tmp_x * tmp_udata.y + spdata.y * tmp_udata.x; 
+                                index_shift  += J;
+                                }; // Iterate over dimensions 1 -> Nd - 1
+                        
+                        float2 ydata=input[myRow]; // kout[col];
+                        u.x =  spdata.x*ydata.x + spdata.y*ydata.y;
+                        u.y =  - spdata.y*ydata.x + spdata.x*ydata.y;
+                        
+                        // atomic_add_float2(k + col*Reps + nc, u);//, res + col*Reps + nc);
+                        atomic_add_float2(k + col*Reps + nc, u, res + col*Reps + nc);
+                        }; // Iterate for (unsigned int j = 0;  j  <  prodJd; j ++)
+        };  // if (m < nRow)
+        
+        };    // End of xELL_spmvh_mCoil    
+    
         KERNEL void pELL_spmvh_mCoil_old(
         const    unsigned int    Reps,             // number of coils
         const    unsigned int    nRow,        // number of rows
@@ -676,7 +747,7 @@ def cSpmvh():
         };    // End of pELL_spmvh_mCoil_old  
         
         
-        KERNEL void pELL_spmvh_mCoil(
+        KERNEL void pELL_spmvh_mCoil_new(
         const    unsigned int    Reps,             // number of coils
         const    unsigned int    nRow,        // number of rows
         const    unsigned int    prodJd,     // product of Jd
